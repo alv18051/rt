bce61c79b139f1eee1101c1872991d7609bf7e23
alv18051 <alv18051@uvg.edu.gt>
2022-10-07 23:44:24 -0600

    final

--- Raytracer.py
+++ Raytracer.py
@@ -1,4 +1,4 @@
-from winreg import REG_FULL_RESOURCE_DESCRIPTOR
+
 from gl import Raytracer, V3
 from texture import *
 from figures import *
@@ -16,7 +16,8 @@ grass = Material(diffuse = (0, 0.5, 0), spec = 8, matType= OPAQUE)
 marble = Material(diffuse = (0.95, 0.95, 0.95), spec = 8, matType= REFLECTIVE)
 
 mirror = Material(diffuse = (0.9, 0, 0.9), spec = 64, matType = REFLECTIVE)
-# mirror2 = Material(diffuse = (0.5, 0.8, 0.9), spec = 64, matType = REFLECTIVE)
+mirror2 = Material(diffuse = (0.5, 0.8, 0.9), spec = 64, matType = TRANSPARENT)
+mirror3 = Material(diffuse = (0.5, 0.8, 0.9), spec = 64, matType = OPAQUE)
 
 water = Material(diffuse = (0.9, 0.9, 0.9), spec = 64, ior = 1.01, matType = OPAQUE)
 yellowSubmarine = Material(diffuse = (0, 0.9, 0.9), spec = 64, ior = 2.45, matType = REFLECTIVE)
@@ -26,19 +27,17 @@ rtx = Raytracer(width, height)
 
 rtx.envMap = Texture("map.bmp")
 
-# rtx.lights.append( AmbientLight(intensity = 0.1 ))
-# rtx.lights.append( DirectionalLight(direction = (-1,-1,-1), intensity = 0.8 ))
+rtx.lights.append( AmbientLight(intensity = 0.1 ))
+rtx.lights.append( DirectionalLight(direction = (-1,-1,-1), intensity = 0.8 ))
 
-rtx.lights.append( PointLight(point = (0, 0, 0)))
-rtx.scene.append(Plane(position= (0,-13,0), normal= (0,1,0), material= yellowSubmarine))
-rtx.scene.append(Plane(position= (0,13,0), normal= (0,1,0), material= yellowSubmarine))
-rtx.scene.append(Plane(position= (50,0,0), normal= (1,0,0), material= wood))
-rtx.scene.append(Plane(position= (-50,0,0), normal= (1,0,0), material= snow))
-rtx.scene.append(Plane(position= (0,0,50), normal= (0,0,1), material= grass))
+# rtx.lights.append( PointLight(point = (0, 0, 0)))
 
-#cajas
-rtx.scene.append(AABB(position=(2,1,-10), size=(2,2,2), material= grass))
-rtx.scene.append(AABB(position=(-2,1,-10), size=(2,2,2), material= mirror))
+rtx.scene.append(Triangle(V3(-1,0,-10), V3(0,1,-10), V3(1,0,-10), material= mirror))
+
+rtx.scene.append(Triangle(V3(-5,0,-10), V3(0,-10,-10), V3(5,0,-10), material= mirror2))
+
+rtx.scene.append(Triangle(V3(3,0,-10), V3(0,5,10), V3(-3,0,-10), material= mirror3))
+
 
 
 rtx.glRender()
